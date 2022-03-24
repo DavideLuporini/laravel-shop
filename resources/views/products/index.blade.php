499c075761974f4b94d4dd3a6e5b77d0e583306c
marcodeiulio <mdeiulio@icloud.com>
2022-03-24 16:15:05 +0100

    - Added edit button in products.index - Added edit button in products.show

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -11,6 +11,7 @@
         <p>{{ $product->description }}</p>
         <div>{{ $product->price }}</div>
         <a href="{{ route('products.show', ['product' => $product->id]) }}">Dettagli</a>
+        <a class="d-block" href="{{ route('products.edit', ['product' => $product->id]) }}">Modifica</a>
     </li>
     @endforeach
 </ul>
